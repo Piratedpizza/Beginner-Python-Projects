d5613ba01c9e4d025e4769d87b6ed4ab6a849715
Tausif <96216775+Piratedpizza@users.noreply.github.com>
2022-04-30 03:04:08 +0100

    Add files via upload

---
+++ Madlibs using f strings.py
@@ -0,0 +1,10 @@
+## Fun with Madlib creating a story using f string
+
+print("let's write a story, I ll give you a plot and you fill the verbs and adjectives")
+punc = input("Enter correct punctuation mark:")
+adj = input("Enter any noun:")
+verb1 = input("Enter a verb:")
+adj2 = input("Enter an adjective:")
+verb2 = input("Enter another verb:")
+
+print(f"I am learning programming and so far it has been wonderful {punc} The {adj} will always be there, so dont't remain {verb1} on them. The {adj2} is to be consistent. Don't {verb2} if things don't go right, remember you need to fall before you can walk.")
